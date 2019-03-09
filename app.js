fc99f1c67c7d458df7ba60d0ae5b7614a3e4f360
gogoyubari <1097517+gogoyubari@users.noreply.github.com>
2019-03-09 22:56:22 +0900

    http-proxy

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-'use strict';
+var httpProxy = require('http-proxy');
 
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var ip = require('ip');
-const PORT = process.env.PORT || 7000;
-var Client = require('node-rest-client').Client;
-var client = new Client();
-
-app.get('/' , function(req, res){
-    client.get("http://www.google.com:80", (data, response) => {
-        res.send(data ? data.toString('utf8') : "ERROR");
-    });
+var proxy = httpProxy.createProxyServer({
+	target: 'http://192.168.1.5:7000',
 });
 
-http.listen(PORT, function(){
-    console.log(`Listening ${ip.address()}:${PORT}. press Ctrl+C to stop.`);
+proxy.listen(8080);
+
+proxy.on('error', function(err, req, res) {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain',
+  });
+  res.end('Something went wrong. And we are reporting a custom error message.');
 });
